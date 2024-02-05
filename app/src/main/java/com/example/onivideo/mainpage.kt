a228c35dd1c7187355435a5c46c7cb089db56f1d
unknown <alifarhad.work@gmail.com>
2024-02-05 12:25:29 +0330

    first commit + navhub

---
+++ app/src/main/java/com/example/onivideo/mainpage.kt
@@ -0,0 +1,9 @@
+package com.example.onivideo
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MainComp(navController: NavController){
+
+}
---
+++ app/src/main/java/com/example/onivideo/navbar.kt
@@ -0,0 +1,12 @@
+package com.example.onivideo
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun NavbarComp(navController: NavController){
+    Row() {
+
+    }
+}
